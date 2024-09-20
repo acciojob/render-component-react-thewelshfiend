ef35e3d7f4c2e13f43896623c9bc542668352904
Kiran TJ <kiran.thomas.jacob@gmail.com>
2024-09-20 09:37:50 +0530

    Commit 1

---
+++ src/components/Comp.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Comp = () => {
+    return (
+        <p>
+            I am learning React. My life is getting better.
+        </p>
+    )
+}
+
+export default Comp
